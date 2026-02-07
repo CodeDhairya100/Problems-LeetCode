c0dfea719df7e743e8a9b6ada9acacb3192248ce
Dhairya Gupta <151992902+CodeDhairya100@users.noreply.github.com>
2026-02-07 14:00:36 +0530

    Time: 0 ms (100.00%) | Memory: 27.2 MB (79.70%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -4,9 +4,7 @@ public:
         int i=0;
         int n=s.size()-1;
         while(i<n){
-            swap(s[i],s[n]);
-            i++;
-            n--;
+            swap(s[i++],s[n--]);
         }
     }
 };
