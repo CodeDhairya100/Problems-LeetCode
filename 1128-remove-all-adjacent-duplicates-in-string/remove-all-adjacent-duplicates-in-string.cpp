1ab747360fa7156666648d7ac9af0737daf59daf
Dhairya Gupta <151992902+CodeDhairya100@users.noreply.github.com>
2026-02-16 21:30:42 +0530

    Time: 2945 ms (5.04%) | Memory: 12.5 MB (96.92%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -3,7 +3,7 @@ public:
     string removeDuplicates(string s) {
         int i=1;
         while(i<s.length()){
-            if(i<=s.length()-1 && s[i]==s[i-1]){
+            if(i<s.length() && s[i]==s[i-1]){
                 s.erase((i-1),2);
                 i=1;
             }
